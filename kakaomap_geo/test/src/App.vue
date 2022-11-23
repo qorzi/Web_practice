44a51a58ce67f74cf2e28e7cb5f8b8da3deebc1a
qorzi <qorzi00@gmail.com>
2022-11-23 09:32:11 +0900

    kakamap build

--- kakaomap_geo/test/src/App.vue
+++ kakaomap_geo/test/src/App.vue
@@ -2,7 +2,8 @@
   <div id="app">
     <nav>
       <router-link to="/">CurrentSite</router-link> |
-      <router-link to="/map">Map</router-link>
+      <router-link to="/map">Map</router-link> |
+      <router-link to="/map2">Map2</router-link>
     </nav>
     <router-view/>
   </div>
--- kakaomap_geo/test/src/router/index.js
+++ kakaomap_geo/test/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import CurrentSite from '../views/CurrentSite.vue'
 import KakaoMap from '@/views/KakaoMap'
+import KakaoMap2 from '@/views/KakaoMap2'
 
 Vue.use(VueRouter)
 
@@ -15,6 +16,11 @@ const routes = [
     path: '/map',
     name: 'map',
     component: KakaoMap
+  },
+  {
+    path: '/map2',
+    name: 'map2',
+    component: KakaoMap2
   }
 ]
 
